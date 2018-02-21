7a4690b318854c202a4435f0b9a41eb14db31cbb
ronamit <Was21235>
2018-02-21 15:42:09 +0200

    update

--- utils.py
+++ utils.py
@@ -1,44 +1,18 @@
-import tensorflow as tf
-from tensorflow.contrib import slim
+
+
+from __future__ import absolute_import, division, print_function
 import numpy as np
-import sys
-from tensorflow.contrib.layers.python.layers import initializers
 
-
-def fc_net(inp, layers, out_layers, scope, lamba=1e-3, activation=tf.nn.relu, reuse=None,
-           weights_initializer=initializers.xavier_initializer(uniform=False)):
-    with slim.arg_scope([slim.fully_connected],
-                        activation_fn=activation,
-                        normalizer_fn=None,
-                        weights_initializer=weights_initializer,
-                        reuse=reuse,
-                        weights_regularizer=slim.l2_regularizer(lamba)):
-
-        if layers:
-            h = slim.stack(inp, slim.fully_connected, layers, scope=scope)
-            if not out_layers:
-                return h
-        else:
-            h = inp
-        outputs = []
-        for i, (outdim, activation) in enumerate(out_layers):
-            o1 = slim.fully_connected(h, outdim, activation_fn=activation, scope=scope + '_{}'.format(i + 1))
-            outputs.append(o1)
-        return outputs if len(outputs) > 1 else outputs[0]
-
-
-def get_y0_y1(sess, y, f0, f1, shape=(), L=1, verbose=True):
-    y0, y1 = np.zeros(shape, dtype=np.float32), np.zeros(shape, dtype=np.float32)
-    ymean = y.mean()
-    for l in range(L):
-        if L > 1 and verbose:
-            sys.stdout.write('\r Sample {}/{}'.format(l + 1, L))
-            sys.stdout.flush()
-        y0 += sess.run(ymean, feed_dict=f0) / L
-        y1 += sess.run(ymean, feed_dict=f1) / L
-
-    if L > 1 and verbose:
-        print
-    return y0, y1
-
-
+def batch_generator(array, batch_size):
+  """Generate batch with respect to array's first axis."""
+  start = 0  # pointer to where we are in iteration
+  while True:
+    stop = start + batch_size
+    diff = stop - array.shape[0]
+    if diff <= 0:
+      batch = array[start:stop]
+      start += batch_size
+    else:
+      batch = np.concatenate((array[start:], array[:diff]))
+      start = diff
+    yield batch
